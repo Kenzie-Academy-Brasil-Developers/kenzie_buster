022689406c5535aea8d403852c1cad0876af66fe
Marchi8 <luandeoliveirasoares@gmail.com>
2022-12-09 12:14:31 -0300

    feat: django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (("Credenciais", {"fields": ("username", "password")}),)
+
+
+admin.site.register(User, CustomUserAdmin)
